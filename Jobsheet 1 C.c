9f1365abae4d963755687a418892fd4a2e5941bc
M-Didik-Cahya-Indirman <64572304+M-Didik-Cahya-Indirman@users.noreply.github.com>
2020-05-17 08:49:26 +0800

    Create Jobsheet 1 C.c
    Program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main (){
+int nim[8]= {1, 2, 3, 4, 5, 6, 7, 8};
+printf ("Nama : M. Didik Cahya Indirman\n");
+printf("kelompok : 18\n");
+printf("NIM : F1B019088\n");
+printf ("Nim ke-1 = %i\n",nim[0]);
+printf ("Nim ke-2 = %i\n",nim[1]);
+printf ("Nim ke-3 = %i\n",nim[2]);
+printf ("Nim ke-4 = %i\n",nim[3]);
+printf ("Nim ke-5 = %i\n",nim[4]);
+printf ("Nim ke-6 = %i\n",nim[5]);
+printf ("Nim ke-7 = %i\n",nim[6]);
+printf ("Nim ke-8 = %i\n",nim[7]);
+}
